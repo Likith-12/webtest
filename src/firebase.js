15302859dc401d13ab29d824543368cf36297d26
sreeroop <sreeroopsk10@gmail.com>
2021-09-30 22:09:37 +0530

    cred added

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,12 @@ import 'firebase/firestore'
 
 
 var firebaseConfig = {
-
+    apiKey: "AIzaSyAEUW9mdm0gQG_dNaMd5wrhxw20X1hsDZw",
+    authDomain: "ipf-website.firebaseapp.com",
+    projectId: "ipf-website",
+    storageBucket: "ipf-website.appspot.com",
+    messagingSenderId: "1067768690265",
+    appId: "1:1067768690265:web:b8aecf3d75642339dc2348"
     //config
 };
 // Initialize Firebase
