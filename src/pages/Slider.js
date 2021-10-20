d949f1990f14335b3b1f02d70c981dbbd73536c6
sidharth48-hub <sidharth.cp48@gmail.com>
2021-10-20 21:11:04 +0530

    slider_responsive

--- src/pages/Slider.js
+++ src/pages/Slider.js
@@ -2,73 +2,119 @@ import React, { useEffect, useState } from 'react'
 
 import './css/slider.scss'
 
+function Slider(){
 
-function Slider() {
-  const [list1, setList1] = useState('active');
-  const [list2, setList2] = useState('');
-  const [list3, setList3] = useState('');
+  const [hrz1,sethrz1] = useState('');
+  const [hrz2,sethrz2] = useState('');
+  const [hrzde1,sethrzde1] = useState('');
+  const [hrzde2,sethrzde2] = useState('');
 
 
+  const [ver1,setver1] = useState('');
+  const [ver2,setver2] = useState('');
+  const [verde1,setverde1] = useState('');
+  const [verde2,setverde2] = useState('');
 
-  function clearActiveClasses() {
-    setList1('')
-    setList2('')
-    setList3('')
+  const { innerWidth: width} = window;
+
+  function clearActiveClasses(){
+      sethrz1('')
+      sethrz2('')
+      sethrzde1('')
+      sethrzde2('')
+
+      setver1('')
+      setver2('')
+      setverde1('')
+      setverde2('')
   }
 
-  function slide_one(event) {
-    clearActiveClasses();
-    setList1('active')
+  function slide_one(){
+      clearActiveClasses();
+      if(width>480)
+      {
+          sethrzde1('deactive_1')
+          sethrzde2('deactive_2')
+      }
 
+      if(width<=480)
+      {
+          setverde1('devertical_1');
+      }
   }
 
-  function slide_two() {
-    clearActiveClasses();
-    setList2('active')
+  function slide_two(){
+      clearActiveClasses();
+      if(width>480)
+      {
+          sethrz1('active_1')
+          sethrzde2('deactive_2')
+      }
+
+      if(width<=480)
+      {
+          setver1('vertical_1')
+          setverde2('devertical_2')
+      }
   }
 
-  function slide_three() {
-    clearActiveClasses();
-    setList3('active')
+  function slide_three(){
+      clearActiveClasses();
+      if(width>480)
+      {
+          sethrz1('active_1')
+          sethrz2('active_2')
+      }
+
+      if(width<=480)
+      {
+          setver1('vertical_1')
+          setver2('vertical_2')
+      }
   }
 
-  return (
-    <div className="container">
-      <div className={`slide ${list1} slide_1`} onClick={slide_one} id="slide_1">
-        <div className="slide_event_1">
-          <a className="event">
-            <span>E</span>
-            <span>V</span>
-            <span>E</span>
-            <span>N</span>
-            <span>T</span>
-            <span>S</span>
+
+
+  return(
+  <div className="container">
+    <div className="slide slide_3">
+      <div className="slide_event_3">  
+          <a id="event_3" onClick={slide_three}>
+             <span>B</span>
+             <span>L</span>
+             <span>O</span>
+             <span>G</span>
           </a>
-        </div>
       </div>
-      <div className={`slide ${list2} slide_2`} onClick={slide_two} id="slide_2">
-        <div className="slide_event_2">
-          <a className="event">
-            <span>S</span>
-            <span>E</span>
-            <span>R</span>
-            <span>I</span>
-            <span>E</span>
-            <span>S</span>
-          </a>
+
+      <div className={`slide slide_2 ${hrz2} ${hrzde2} ${ver2} ${verde2}`}>
+          <div className="slide_event_2">  
+              <a id="event_2" onClick={slide_two}>
+                 <span>S</span>
+                 <span>E</span>
+                 <span>R</span>
+                 <span>I</span>
+                 <span>E</span>
+                 <span>S</span>
+              </a>
+          </div>
+
+          <div className={`slide slide_1 ${hrz1} ${hrzde1} ${ver1} ${verde1}`}>
+              <div className="slide_event_1">  
+                <a id="event_1" onClick={slide_one}>
+                   <span>E</span>
+                   <span>V</span>
+                   <span>E</span>
+                   <span>N</span>
+                   <span>T</span>
+                   <span>S</span>
+                </a>
+              </div>
+          </div>
         </div>
-      </div>
-      <div className={`slide slide_3 ${list3}`} onClick={slide_three} id="slide_3">
-        <div className="slide_event_3">
-          <a className="event">
-            <span>B</span>
-            <span>L</span>
-            <span>O</span>
-            <span>G</span>
-          </a>
-        </div>
-      </div>
-    </div>
+    </div>         
+  </div>
   );
 }
+
 export default Slider;
