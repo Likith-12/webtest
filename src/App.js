d0f16ed1426701269ec58652d09ac9b14c9e3ba4
sreeroop <sreeroopsk10@gmail.com>
2021-10-27 02:50:20 +0530

    old 3d model removed, serieses and blogs modified

--- src/App.js
+++ src/App.js
@@ -4,28 +4,27 @@ import Home from './pages/Home';
 import Blogs from './pages/Blogs';
 import Blog from './pages/Blog';
 import Events from './pages/Events';
-// import AddPost from './admin/AddPost';
 import './App.scss';
 import Navbar from './components/Navbar';
-import Admin from './admin/Admin';
 import AnimatedCursor from "react-animated-cursor"
 import Team from './pages/Team';
+import Serieses from './pages/Serieses';
 
 function App() {
 
 
   return (
     <div className="App">
-        
-       <AnimatedCursor
-      innerSize={30}
-      outerSize={15}
-      color='50, 171, 150'
-      outerAlpha={0.5}
-      outerScale={3}
-      trailingSpeed={10}
-      
-    />
+
+      <AnimatedCursor
+        innerSize={30}
+        outerSize={15}
+        color='50, 171, 150'
+        outerAlpha={0.5}
+        outerScale={3}
+        trailingSpeed={10}
+
+      />
       <Router>
         <Navbar />
         <Switch>
@@ -41,16 +40,13 @@ function App() {
           <Route path="/events">
             <Events />
           </Route>
-          <Route path="/admin">
-            <Admin />
-          </Route>
           <Route path="/team">
-           <Team></Team>
+            <Team></Team>
           </Route>
-          <Route path="/events">
-            <Events />
+          <Route path="/serieses">
+            <Serieses />
           </Route>
-          
+
         </Switch>
       </Router>
     </div>
