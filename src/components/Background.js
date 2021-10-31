04baf5ab12af546659411a3de344cb03962b5303
sidharth48-hub <sidharth.cp48@gmail.com>
2021-10-31 14:00:35 +0530

    background animation changes

--- src/components/Background.js
+++ src/components/Background.js
@@ -5,8 +5,8 @@ const Background = () => {
     return (
         <>
           <div id="stars"></div>
-          {/* <div id="stars2"></div>
-          <div id="stars3"></div>   */}
+          <div id="stars2"></div>
+          <div id="stars3"></div>  
         </>
     )
 }
