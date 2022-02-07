ebd501d9316479f7567dbe9a258c757f85a709e1
sivuu96 <jsivsankar@gmail.com>
2022-02-07 17:44:01 +0530

    insta cards

---
+++ src/pages/Series.js
@@ -0,0 +1,12 @@
+import InstaCards from '../components/InstaCards';
+import React from "react";
+
+function Series() {
+  return (
+    
+        <InstaCards />
+    
+  );
+};
+
+export default Series;
