b58fc34489a78ce91ddb3551ebb39846be5098bf
sreeroop <sreeroopsk10@gmail.com>
2022-05-17 12:16:04 +0530

    styling bug fix

--- src/components/Background.js
+++ src/components/Background.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 import './css/background.scss'
 const Background = () => {
-    return (
-        <>
-          <div id="stars"></div>
-          <div id="stars2"></div>
-          <div id="stars3"></div>  
-        </>
-    )
+  return (
+    <div className='bgContainer'>
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+    </div>
+  )
 }
 
 export default Background
