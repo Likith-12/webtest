7b0ca75a3b285e4943270428b19f9e23f42238be
sidharth48-hub <sidharth.cp48@gmail.com>
2022-06-09 00:07:50 +0530

    changes made in footer

---
+++ src/components/Contact.js
@@ -0,0 +1,59 @@
+import './css/contact.scss';
+import React,{useState,useEffect} from 'react';
+
+const Contact = ({}) =>{
+    const [formData, setFormData] = useState({});
+  const [message, setMessage] = useState("");
+
+  const handleInput = e => {
+    const copyFormData = { ...formData };
+    copyFormData[e.target.name] = e.target.value;
+    setFormData(copyFormData);
+  };
+
+  const sendData = async e => {
+    e.preventDefault();
+    const { name, email, contact_no, message } = formData
+    try {
+      const response = await fetch(
+        "https://v1.nocodeapi.com/scp/google_sheets/HwERcrTVEeAxzfrz?tabId=ipf",
+        {
+          method: "post",
+          body: JSON.stringify([[name, email, contact_no, message]]),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      );
+      const json = await response.json();
+      console.log("Success:", JSON.stringify(json));
+      setMessage("Success");
+    } catch (error) {
+      console.error("Error:", error);
+      setMessage("Error");
+    }
+  };
+    return(
+<div className="container">  
+  <form id="contact" onSubmit={sendData}>
+    <h3>Contact</h3>
+    <fieldset>
+      <input placeholder="Your name" type="text" tabindex="1" name="name"required autofocus onChange={handleInput}/>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your Email Address" type="email" name="email"tabindex="2" required onChange={handleInput}/>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your Phone Number" type="tel" name="contact_no"tabindex="3" required onChange={handleInput}/>
+    </fieldset>
+    <fieldset>
+      <textarea placeholder="Type your Message Here...." name="message"tabindex="5" required onChange={handleInput}></textarea>
+    </fieldset>
+    <fieldset>
+      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+    </fieldset>
+  </form>
+</div>
+    )
+}
+export default Contact;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 
 import Footer_ring from '../components/Footer_ring'
+import Contact from '../components/Contact';
 
 import './css/footer.scss'
+
+
 function Footer(){
+
+    const [show,setshow] = useState(true);
+
     return(
         <div className="footer">
-        <div className="footer_text">
+        <div>
+        {show?(<div className="footer_text" onClick={() => {setshow(false);}}>
             FEEL FREE TO HIT US UP
+            </div>):(<div className="contact"><Contact/></div>)}    
         </div>
         {/* <div className="footer_copyright">
                     <span>Copyright
