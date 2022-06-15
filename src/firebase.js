86727d792257aedc65a7904757a056d5f53deddc
sreeroop <sreeroopsk10@gmail.com>
2022-06-15 23:40:40 +0530

    prod cred added

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,12 @@
 import firebase from "firebase/app";
 import 'firebase/firestore'
-var firebaseConfig = {
-    apiKey: "AIzaSyAEUW9mdm0gQG_dNaMd5wrhxw20X1hsDZw",
-    authDomain: "ipf-website.firebaseapp.com",
-    projectId: "ipf-website",
-    storageBucket: "ipf-website.appspot.com",
-    messagingSenderId: "1067768690265",
-    appId: "1:1067768690265:web:b8aecf3d75642339dc2348"
-    //config
+const firebaseConfig = {
+    apiKey: "AIzaSyCkUNhxV9irixJ9hQGxx5mTtVs8ll4F2B0",
+    authDomain: "ipf-website-official.firebaseapp.com",
+    projectId: "ipf-website-official",
+    storageBucket: "ipf-website-official.appspot.com",
+    messagingSenderId: "660111767947",
+    appId: "1:660111767947:web:2afbf6e16fff2ced45178e"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
